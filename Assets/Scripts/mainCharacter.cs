80e16ce22a16ba26d23e5758206ec99041cf362f
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2024-02-12 17:42:45 +0100

    movement + health

--- Assets/Scripts/mainCharacter.cs
+++ Assets/Scripts/mainCharacter.cs
@@ -2,19 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mainCharacterMovement : MonoBehaviour
+public class mainCharacter : MonoBehaviour
 {
+    //vars
+
+    //Movement vars
     public float speed;
     private float moveX;
     private float moveY;
 
+    //Health vars
+    public float startingHealth;
+    private float totalCurrentHealth;
+
     void Start()
     {
+        //Movement speed
         speed = 4;
+
+        //Starting health = Current health
+        startingHealth = 100;
+        totalCurrentHealth = startingHealth;
     }
 
     void FixedUpdate()
     {
+        Movement();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void Movement()
+    {
+        //Movement
         moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.position += new Vector3(moveX, moveY, 0);
