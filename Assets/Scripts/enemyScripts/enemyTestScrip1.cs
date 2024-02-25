4de127e637b2553c790158d0470667dcb5da404a
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-02-25 16:26:28 +0100

    commenting enemy attack
    commenting enemy attack code untill attack is worked out for player

--- Assets/Scripts/enemyScripts/enemyTestScrip1.cs
+++ Assets/Scripts/enemyScripts/enemyTestScrip1.cs
@@ -6,32 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class enemyTestScrip1 : MonoBehaviour
 {
-    public string state = "State.Idle";
+    public string state = "State.Idle"; //what does the enemy want to do
 
     private Transform player;
     public float distanceToPlayer;
 
-    private float speed = 2f;
-    private float closeEnough = 3f;
-    private float toClose = 2f;
-    private float timer = 0f;
-    private float moveTime = 0.1f;
-    private float engageDistance = 10f;
+    private float speed = 2f; //movement speed
+    private float closeEnough = 3f; //how close does the enemy want to get
+    private float toClose = 2f; //how far does the enemy want to stay away from player
+    private float timer = 0f; //timer to keep track of time before moving
+    private float moveTime = 0.1f; //time to start moving
+    private float engageDistance = 10f; //at what distance should the enemy start going to the player
 
-    //attack
-    public float rotationCheck;
-    private float angle;
+    ////attack (work in progress)
+    //public float rotationCheck;
+    //private float angle;
 
-    private GameObject spawnedBullet;
-    public static GameObject spawnedMousePointer;
-    public GameObject mousePointer;
-    private Vector2 worldMousePosition;
-    private Vector2 mousePosition;
+    //private GameObject spawnedBullet;
+    //public static GameObject spawnedMousePointer;
+    //public GameObject mousePointer;
+    //private Vector2 worldMousePosition;
+    //private Vector2 mousePosition;
 
-    private float timerArchers;
-    private float canFireArchers = 0.5f;
-    public GameObject arrow;
-    public Transform arrowSpawnPoint;
+    //private float timerArchers;
+    //private float canFireArchers = 0.5f;
+    //public GameObject arrow;
+    //public Transform arrowSpawnPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -43,34 +43,34 @@ public class enemyTestScrip1 : MonoBehaviour
     void Update()
     {
         distanceToPlayer = Vector2.Distance(transform.position, player.position);
-        StateConditions();
-        ExecuteConditions();
+        StateConditions(); //check what should the enemy should want to do
+        ExecuteConditions(); //try to do what the enemy should want to do
     }
 
     public void StateConditions()
     {
-        if (distanceToPlayer <= engageDistance)
+        if (distanceToPlayer <= engageDistance) //check if the enemy is within range of the player
         {
 
-            if (distanceToPlayer >= closeEnough)
+            if (distanceToPlayer >= closeEnough) //move to the player if far away
             {
                 state = "State.Move";
             }
-            else if (distanceToPlayer <= toClose)
+            else if (distanceToPlayer <= toClose) //move away from player if to close
             {
                 state = "State.Retreat";
             }
-            else
+            else //reset move time if within range to attack player
             {
                 timer = 0f;
             }
 
-            if (distanceToPlayer < closeEnough && distanceToPlayer > toClose)
+            if (distanceToPlayer < closeEnough && distanceToPlayer > toClose) //attack player if within the right range to do so
             {
                 state = "State.attack";
-                timerArchers += Time.deltaTime;
+                //timerArchers += Time.deltaTime;
             }
-        } else
+        } else //idle if the enemy is out of range of the player
         {
             state = "State.Idle";
         }
@@ -79,12 +79,12 @@ public class enemyTestScrip1 : MonoBehaviour
     {
         switch (state)
         {
-            case "State.Idle":
+            case "State.Idle": //idle 
                 Debug.Log("State.Idle");
                 break;
             case "State.attack":
                 Debug.Log("State.Attack");
-                Attack();
+                //Attack();
                 break;
             case "State.Move":
                 Move();
@@ -113,15 +113,15 @@ public class enemyTestScrip1 : MonoBehaviour
             transform.Translate((player.position - transform.position).normalized * Time.deltaTime * -speed);
         }
     }
-    public void Attack()
-    {
-        if (timerArchers >= canFireArchers)
-        {
-            spawnedBullet = arrow;
-            spawnedMousePointer = Instantiate(mousePointer, worldMousePosition, Quaternion.Euler(0, 0, angle));
-            spawnedBullet = Instantiate(arrow, arrowSpawnPoint.position, Quaternion.Euler(0, 0, angle));
+    //public void Attack()
+    //{
+    //    if (timerArchers >= canFireArchers)
+    //    {
+    //        spawnedBullet = arrow;
+    //        spawnedMousePointer = Instantiate(mousePointer, worldMousePosition, Quaternion.Euler(0, 0, angle));
+    //        spawnedBullet = Instantiate(arrow, arrowSpawnPoint.position, Quaternion.Euler(0, 0, angle));
 
-            timerArchers = 0f;
-        }
-    }
+    //        timerArchers = 0f;
+    //    }
+    //}
 }
--- Assets/Scripts/enemyScripts/enemyTestScript2.cs
+++ Assets/Scripts/enemyScripts/enemyTestScript2.cs
@@ -4,41 +4,41 @@ using UnityEngine;
 
 public class enemyTestScript2 : MonoBehaviour
 {
-    public GameObject player;
-    public static float speed = 10f;
-    public static float heightNum = 0.5f;
-    public Vector3 movePosition;
-    private float playerX;
-    private float targetX;
-    private float nextX;
-    private float dist;
-    private float baseY;
-    private float height;
+//    public GameObject player;
+//    public static float speed = 10f;
+//    public static float heightNum = 0.5f;
+//    public Vector3 movePosition;
+//    private float playerX;
+//    private float targetX;
+//    private float nextX;
+//    private float dist;
+//    private float baseY;
+//    private float height;
 
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("mainCharacter");
-    }
+//    void Start()
+//    {
+//        player = GameObject.FindGameObjectWithTag("mainCharacter");
+//    }
 
-    void Update()
-    {
-        playerX = player.transform.position.x;
-        targetX = mousePointerPosition.target.transform.position.x;
-        dist = targetX - playerX;
-        nextX = Mathf.MoveTowards(transform.position.x, targetX, speed * Time.deltaTime);
-        baseY = Mathf.Lerp(player.transform.position.y, mousePointerPosition.target.transform.position.y, (nextX - playerX) / dist);
-        height = heightNum * (nextX - playerX) * (nextX - targetX) / (-0.25f * dist * dist);
-        movePosition = new Vector3(nextX, baseY + height, transform.position.z);
-        transform.rotation = LookAtTarget(movePosition - transform.position);
-        transform.position = movePosition;
-        if (movePosition == mousePointerPosition.target.transform.position)
-        {
-            Destroy(gameObject);
-            Destroy(shootingScript.spawnedMousePointer);
-        }
-    }
-    public static Quaternion LookAtTarget(Vector2 r)
-    {
-        return Quaternion.Euler(0, 0, Mathf.Atan2(r.y, r.x) * Mathf.Rad2Deg);
-    }
+//    void Update()
+//    {
+//        playerX = player.transform.position.x;
+//        targetX = mousePointerPosition.target.transform.position.x;
+//        dist = targetX - playerX;
+//        nextX = Mathf.MoveTowards(transform.position.x, targetX, speed * Time.deltaTime);
+//        baseY = Mathf.Lerp(player.transform.position.y, mousePointerPosition.target.transform.position.y, (nextX - playerX) / dist);
+//        height = heightNum * (nextX - playerX) * (nextX - targetX) / (-0.25f * dist * dist);
+//        movePosition = new Vector3(nextX, baseY + height, transform.position.z);
+//        transform.rotation = LookAtTarget(movePosition - transform.position);
+//        transform.position = movePosition;
+//        if (movePosition == mousePointerPosition.target.transform.position)
+//        {
+//            Destroy(gameObject);
+//            Destroy(shootingScript.spawnedMousePointer);
+//        }
+//    }
+//    public static Quaternion LookAtTarget(Vector2 r)
+//    {
+//        return Quaternion.Euler(0, 0, Mathf.Atan2(r.y, r.x) * Mathf.Rad2Deg);
+//    }
 }
