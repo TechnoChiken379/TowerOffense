8ea071fa5ba3ac059fe6b4a3889ad55c5f014a13
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-02-29 12:04:16 +0100

    added resource script

---
+++ Assets/Scripts/extraAndOrWorldScrips/resources.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resources : MonoBehaviour
+{
+    //var
+    public static int wood = 10;
+    public static int stone = 0;
+    public static int steel = 0;
+    public static int gold = 0;
+
+    //text
+    public TMPro.TMP_Text woodText;
+    public TMPro.TMP_Text stoneText;
+    public TMPro.TMP_Text steelText;
+    public TMPro.TMP_Text goldText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        woodText.text = wood.ToString();
+        stoneText.text = stone.ToString();
+        steelText.text = steel.ToString();
+        goldText.text = gold.ToString();
+
+    }
+}
