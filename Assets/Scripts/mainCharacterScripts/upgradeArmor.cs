4219fb3a34eeeb981b9c8768dda14ca3f0f0b87c
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-03-05 16:38:08 +0100

    working on upgrades

--- Assets/Scripts/mainCharacterScripts/upgradeArmor.cs
+++ Assets/Scripts/mainCharacterScripts/upgradeArmor.cs
@@ -10,12 +10,12 @@ public class upgradeArmor : MonoBehaviour
     public static bool healthHeavyTank = false;
     public static bool healthLightTank = false;
 
-    public static float maxHealth = 100f;
+    public static float maxHealth;
 
     //armor
     public static int shieldLevel = 0;
-    public static bool shielHeavyArmor = false;
-    public static bool shielLightArmor = false;
+    public static bool shieldHeavyArmor = false;
+    public static bool shieldLightArmor = false;
 
     public static float maxShieldHealth = 0f;
 
@@ -31,7 +31,9 @@ public class upgradeArmor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        HealthStart();
+        ShieldStart();
+        SelfRepairStart();
     }
 
     // Update is called once per frame
@@ -39,4 +41,140 @@ public class upgradeArmor : MonoBehaviour
     {
         
     }
+    #region start
+    void HealthStart()
+    {
+        if (healthLevel == 1) 
+        {
+            maxHealth = 100f;
+        } 
+        else if (healthLevel == 2)
+        {
+            maxHealth = 200f;
+        }
+        else if (healthLevel == 3)
+        {
+            maxHealth = 300f;
+        }
+        else if (healthLevel == 4)
+        {
+            maxHealth = 400f;
+
+            if (healthHeavyTank == true)
+            {
+
+            }
+            if (healthLightTank == true)
+            {
+
+            }
+        }
+        else if (healthLevel == 5)
+        {
+            maxHealth = 500f;
+
+            if (healthHeavyTank == true)
+            {
+
+            }
+            if (healthLightTank == true)
+            {
+
+            }
+        }
+    }
+
+    void ShieldStart()
+    {
+        if (shieldLevel == 1)
+        {
+            maxShieldHealth = 25f;
+        }
+        else if (shieldLevel == 2)
+        {
+            maxShieldHealth = 50f;
+        }
+        else if (shieldLevel == 3)
+        {
+            maxShieldHealth = 75f;
+        }
+        else if (shieldLevel == 4)
+        {
+            maxShieldHealth = 100f;
+
+            if (shieldHeavyArmor == true)
+            {
+
+            }
+            if (shieldLightArmor == true)
+            {
+
+            }
+        }
+        else if (shieldLevel == 5)
+        {
+            maxShieldHealth = 125f;
+
+            if (shieldHeavyArmor == true)
+            {
+
+            }
+            if (shieldLightArmor == true)
+            {
+
+            }
+        }
+    }
+
+    void SelfRepairStart()
+    {
+        if (selfRepairLevel == 1)
+        {
+            repairCompensation = 0.20f;
+            repairTime = 2.5f;
+        }
+        else if (selfRepairLevel == 2)
+        {
+            repairCompensation = 0.25f;
+            repairTime = 4.375f;
+        }
+        else if (selfRepairLevel == 3)
+        {
+            repairCompensation = 0.30f;
+            repairTime = 6.25f;
+        }
+        else if (selfRepairLevel == 4)
+        {
+            repairCompensation = 0.35f;
+            repairTime = 8.125f;
+
+            if (selfRepairHeavyRepair == true)
+            {
+
+            }
+            if (selfRepairLightRepair == true)
+            {
+
+            }
+        }
+        else if (selfRepairLevel == 5)
+        {
+            repairCompensation = 0.40f;
+            repairTime = 10f;
+
+            if (selfRepairHeavyRepair == true)
+            {
+
+            }
+            if (selfRepairLightRepair == true)
+            {
+
+            }
+        }
+    }
+    #endregion
+
+    #region update
+
+    #endregion
 }
