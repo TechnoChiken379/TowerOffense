b437ab1b4684ccb9cfa447b799a9fa2840db163c
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2024-03-19 16:28:37 +0100

    giving up on deathscreen

--- Assets/Scripts/extraAndOrWorldScrips/deathScreenAnimation.cs
+++ Assets/Scripts/extraAndOrWorldScrips/deathScreenAnimation.cs
@@ -8,29 +8,18 @@ public class deathScreenAnimation : MonoBehaviour
     public Animator DeathScreenAnimator;
 
     private float timer = 0f;
-    private float timer2 = 0f;
     private bool Switch = false;
-    private bool Switch2 = false;
-    private bool Switch3 = false;
+
     public GameObject DeathScreenBackGround;
 
     void Update()
     {
         DeathManager();
         timer += Time.deltaTime;
-        timer2 += Time.deltaTime;
         if (timer >= 4 && Switch == true)
         {
             SceneManager.LoadScene("MainMenu");
             Switch = false;
-        }
-
-        if (timer2 >= 1.5f && Switch2 == true)
-        {
-            Debug.Log("D");
-            DeathScreenBackGround.SetActive(true);
-            Switch2 = false;
-            Switch3 = true;
         }
     }
 
@@ -40,12 +29,7 @@ public class deathScreenAnimation : MonoBehaviour
         if (mainCharacter.totalCurrentHealth <= 0 && !(DeathScreenAnimator.GetBool("MainMenu") == true || DeathScreenAnimator.GetBool("LastSave") == true))
         {
             DeathScreenAnimator.SetBool("Is Dead", true);
-            if (Switch3 == true)
-            {
-                timer2 = 0;
-            }
-            Switch2 = true;
-            Switch3 = false;
+            DeathScreenBackGround.SetActive(true);
         }
         else
         {
