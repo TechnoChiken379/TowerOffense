9abb063ffdeedd3762cbc8cddbd08fa9f719d4cc
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-06-12 15:18:35 +0200

    optimised wall scripts

--- Assets/Scripts/enemyScripts/testOrExample/buildings/walls/enemyWWallFunction.cs
+++ Assets/Scripts/enemyScripts/testOrExample/buildings/walls/enemyWWallFunction.cs
@@ -9,29 +9,8 @@ using UnityEngine.UIElements;
 
 public class enemyWWallFunction : MonoBehaviour
 {
-    public string state = "State.Idle"; //what does the enemy want to do
-
-    private Transform player;
-    public float distanceToPlayer;
     //health
     private float enemyHP, enemyMaxHP = 100f;
-
-    //death drop
-    public GameObject deathDropGold;
-    public GameObject deathDropWood;
-    public GameObject deathDropStone;
-    public GameObject deathDropSteel;
-
-    //private float DroppedGold = 0;
-    //private float DroppedWood = 0;
-    //private float DroppedStone = 0;
-    //private float DroppedSteel = 0;
-
-    public Transform deathDropPoint;
-
-    //spreat out from other enemies
-    private GameObject[] enemies;
-    private Transform closestEnemy;
 
     //encampment
     public GameObject Encampment;
@@ -49,93 +28,20 @@ public class enemyWWallFunction : MonoBehaviour
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("mainCharacter").transform;
-
         enemyHP = enemyMaxHP;
 
         CalculateLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        distanceToPlayer = Vector2.Distance(transform.position, player.position);
-        FindClosestEnemies(); //locate closest enemy
         IfDeadDie();
-    }
-
-    void FindClosestEnemies()
-    {
-        enemies = GameObject.FindGameObjectsWithTag("Enemy");
-
-        closestEnemy = GetClosestEnemy(enemies);
-    }
-
-    Transform GetClosestEnemy(GameObject[] enemiesArray)
-    {
-        float closestDistance = Mathf.Infinity;
-
-        foreach (GameObject enemy in enemiesArray)
-        {
-            if (enemy != gameObject)
-            {
-                float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
-
-                if (distanceToEnemy < closestDistance)
-                {
-                    closestDistance = distanceToEnemy;
-                    closestEnemy = enemy.transform;
-                }
-            }
-        }
-
-        return closestEnemy;
     }
 
     void IfDeadDie()
     {
         if (enemyHP <= 0)
         {
-            ////gold
-            //GameObject enemyDroppedGold = Instantiate(deathDropGold, deathDropPoint.position, Quaternion.identity);
-            //enemyDeathDrop DeathDropGoldScript = enemyDroppedGold.GetComponent<enemyDeathDrop>();
-
-            //if (DeathDropGoldScript != null)
-            //{
-            //    DeathDropGoldScript.SetEnemyScriptReference(this);
-            //    DeathDropGoldScript.DetermineAmountGold(DroppedGold);
-            //}
-
-            ////wood
-            //GameObject enemyDroppedWood = Instantiate(deathDropWood, deathDropPoint.position, Quaternion.identity);
-            //enemyDeathDrop DeathDropWoodScript = enemyDroppedWood.GetComponent<enemyDeathDrop>();
-
-            //if (DeathDropWoodScript != null)
-            //{
-            //    DeathDropWoodScript.SetEnemyScriptReference(this);
-            //    DeathDropWoodScript.DetermineAmountWood(DroppedWood);
-            //}
-
-            ////stone
-            //GameObject enemyDroppedStone = Instantiate(deathDropStone, deathDropPoint.position, Quaternion.identity);
-            //enemyDeathDrop DeathDropStoneScript = enemyDroppedStone.GetComponent<enemyDeathDrop>();
-
-            //if (DeathDropStoneScript != null)
-            //{
-            //    DeathDropStoneScript.SetEnemyScriptReference(this);
-            //    DeathDropStoneScript.DetermineAmountStone(DroppedStone);
-            //}
-
-            ////steel
-            //GameObject enemyDroppedSteel = Instantiate(deathDropSteel, deathDropPoint.position, Quaternion.identity);
-            //enemyDeathDrop DeathDropSteelScript = enemyDroppedSteel.GetComponent<enemyDeathDrop>();
-
-            //if (DeathDropSteelScript != null)
-            //{
-            //    DeathDropSteelScript.SetEnemyScriptReference(this);
-            //    DeathDropSteelScript.DetermineAmountSteel(DroppedSteel);
-            //}
-
             Destroy(gameObject);
         }
     }
